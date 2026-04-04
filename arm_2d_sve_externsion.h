2736ca6f6828ccc1064ce6d0d966ef7bf341d9de
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2026-04-04 03:40:39 +0100

    Update arm_2d_sve_externsion.h

--- arm_2d_sve_externsion.h
+++ arm_2d_sve_externsion.h
@@ -44,17 +44,17 @@ void svst4u8_u16(   svbool_t vPred,
                     svuint16x4_t *pvHigh)
 {
 
-    svuint8_t vCH0u8 = svuzp1_u8( svreinterpret_u8(svget4_u16(*pvLow, 0)),
-                                svreinterpret_u8(svget4_u16(*pvHigh, 0)));
+    svuint8_t vCH0u8 = svuzp1_u8(   svreinterpret_u8(svget4_u16(*pvLow, 0)),
+                                    svreinterpret_u8(svget4_u16(*pvHigh, 0)));
 
-    svuint8_t vCH1u8 = svuzp1_u8( svreinterpret_u8(svget4_u16(*pvLow, 1)),
-                                svreinterpret_u8(svget4_u16(*pvHigh, 1)));
+    svuint8_t vCH1u8 = svuzp1_u8(   svreinterpret_u8(svget4_u16(*pvLow, 1)),
+                                    svreinterpret_u8(svget4_u16(*pvHigh, 1)));
     
-    svuint8_t vCH2u8 = svuzp1_u8( svreinterpret_u8(svget4_u16(*pvLow, 2)),
-                                svreinterpret_u8(svget4_u16(*pvHigh, 2)));
+    svuint8_t vCH2u8 = svuzp1_u8(   svreinterpret_u8(svget4_u16(*pvLow, 2)),
+                                    svreinterpret_u8(svget4_u16(*pvHigh, 2)));
 
-    svuint8_t vCH3u8 = svuzp1_u8( svreinterpret_u8(svget4_u16(*pvLow, 3)),
-                                svreinterpret_u8(svget4_u16(*pvHigh, 3)));
+    svuint8_t vCH3u8 = svuzp1_u8(   svreinterpret_u8(svget4_u16(*pvLow, 3)),
+                                    svreinterpret_u8(svget4_u16(*pvHigh, 3)));
 
     svst4_u8(vPred, pchTarget, svcreate4_u8(vCH0u8, vCH1u8, vCH2u8, vCH3u8));
 }
