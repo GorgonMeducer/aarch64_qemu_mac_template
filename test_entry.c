6d83fbdd63bb8830d74f8e8bb468fcf9b3565aae
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2026-05-04 11:00:41 +0800

    add tests

--- test_entry.c
+++ test_entry.c
@@ -8,10 +8,77 @@
 #include <assert.h>
 #include <stdlib.h>
 
-#include "sve_util.h"
-#include "arm_2d_sve_extension.h"
+//#include "sve_util.h"
+//#include "arm_2d_sve_extension.h"
 
+#if 0
+#undef sdl_sve_rgb32_blend_op_fill_alpha
+#define sdl_sve_rgb32_blend_op_fill_alpha(ma_alpha_chn_idx)                     \
+            if (sve_src_chn_idx == (ma_alpha_chn_idx)) {                        \
+                /* fill alpha */                                                \
+                sve_target_u16 = svdup_u16(0xFF);                               \
+            } else {                                                            \
+                svuint16_t vMask = svget4(sve_source_u16x4, (ma_alpha_chn_idx));\
+                sve_target_u16                                                  \
+                    = sdl_sve_chn_blend_with_mask(  sve_source_u16,             \
+                                                    sve_target_u16,             \
+                                                    vMask);                     \
+            }
 
+#undef sdl_sve_rgb32_blend_op_copy_alpha
+#define sdl_sve_rgb32_blend_op_copy_alpha(ma_alpha_chn_idx)                     \
+            if (sve_src_chn_idx == (ma_alpha_chn_idx)) {                        \
+                svuint16_t vMask = svget4(sve_source_u16x4, (ma_alpha_chn_idx));\
+                sve_target_u16                                                  \
+                    = sdl_sve_chn_blend_with_mask(  svdup_u16(0xFF),            \
+                                                    sve_target_u16,             \
+                                                    vMask);                     \
+            } else {                                                            \
+                svuint16_t vMask = svget4(sve_source_u16x4, (ma_alpha_chn_idx));\
+                sve_target_u16                                                  \
+                    = sdl_sve_chn_blend_with_mask(  sve_source_u16,             \
+                                                    sve_target_u16,             \
+                                                    vMask);                     \
+            }
+
+#undef sdl_sve_rgb32_blend_to_rgb565_op
+#define sdl_sve_rgb32_blend_to_rgb565_op(ma_alpha_chn_idx)                      \
+        do {                                                                    \
+            svuint16_t vMask = svget4(sve_source_u16x4, (ma_alpha_chn_idx));    \
+                sve_target_u16                                                  \
+                    = sdl_sve_chn_blend_with_mask(  sve_source_u16,             \
+                                                    sve_target_u16,             \
+                                                    vMask);                     \
+        } while(0)
+#else
+
+#undef sdl_sve_rgb32_blend_op_fill_alpha
+#define sdl_sve_rgb32_blend_op_fill_alpha(ma_alpha_chn_idx)                     \
+        do {                                                                    \
+            if (sve_src_chn_idx == (ma_alpha_chn_idx)) {                        \
+                /* fill alpha */                                                \
+                sve_target_u16 = svdup_u16(0xFF);                               \
+            } else {                                                            \
+                sve_target_u16 = sve_source_u16;                                \
+            }                                                                   \
+        } while(0)
+
+#undef sdl_sve_rgb32_blend_op_copy_alpha
+#define sdl_sve_rgb32_blend_op_copy_alpha(ma_alpha_chn_idx)                     \
+        do {                                                                    \
+                sve_target_u16 = sve_source_u16;                                \
+        } while(0)
+
+#undef sdl_sve_rgb32_blend_to_rgb565_op
+#define sdl_sve_rgb32_blend_to_rgb565_op(ma_alpha_chn_idx)                      \
+        do {                                                                    \
+            sve_target_u16 = sve_source_u16;                                    \
+            SVT_PRINT_VECTOR(sve_target_u16, uint16_t, "%04x");                 \
+        } while(0)
+
+#endif
+
+#include "SDL_sve2_swizzle.h"
 
 
 __attribute__((nonnull(1,2)))
@@ -26,38 +93,6 @@ int main(void) {
 
     printf("SVE Vector Length: %zu bits\r\n", svlen(svundef_u8()) * 8);
 
-#if 0
-    svuint16_t v16Input = svundef_u16();
-    svuint16_t v16Input2 = svundef_u16();
-    svuint32_t v32Input3 = svundef_u32();
-
-    svbool_t vPred1 = svptrue_pat_b16(SV_VL1);
-    svbool_t vPred2 = svptrue_pat_b16(SV_VL6);
-    //svbool_t vPred3 = svdup_n_b8(true);
-
-    //vPred1 &= vPred2;
-
-    vPred2 &= ~vPred1;
-
-    
-
-    printf("[%08x]\r\n", *(uint32_t *)&vPred2);
-
-    //SVT_INIT_PRED(vPred, 0xFF, 0x10, 0x00, 0x00);
-
-    SVT_PRINT_PRED(vPred2, uint16_t);
-
-    SVT_INIT_VECOTR(v16Input, uint16_t, 1, 2, 3, 4, 5, 6, 7, 8, 9,10);
-    SVT_INIT_VECOTR(v16Input2, uint16_t, 0xFFFF, 0xFFFF, 0xAAAA);
-    SVT_INIT_VECOTR(v32Input3, uint32_t, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 , 16);
-
-    v16Input2 += 1; //v16Input;
-
-    SVT_PRINT_VECTOR(v16Input, uint16_t, "0x%04"PRIx16);
-    SVT_PRINT_VECTOR(v16Input2, uint16_t, "0x%04"PRIx16);
-    SVT_PRINT_VECTOR(v32Input3, uint32_t, "0x%08"PRIx32);
-
-#endif
 
 #define PIXEL_COUNT             128
 
@@ -94,47 +129,39 @@ int main(void) {
         pchTargetMask[n] = 0xFF;
     }
 
-#if 0
-    __arm_2d_sve_cccn888_blend_with_opacity((uint32_t *)pchSource, 
-                                            (uint32_t *)pchTarget, 
-                                            OUTPUT_BUFFER_SIZE / sizeof(uint32_t), 
-                                            128);
-
-    
-    __arm_2d_sve_cccn888_blend_with_masks(  (uint32_t *)pchSource, 
-                                            (uint32_t *)pchTarget, 
-                                            pchMask,
-                                            pchMask,
-                                            OUTPUT_BUFFER_SIZE / sizeof(uint32_t));
-
-    __arm_2d_sve_cccn888_fill_colour_with_source_masks((uint32_t *)pchTarget, pchMask, pchMask, 17, 0x00804020);
-    
-    __arm_2d_sve_cccn888_reverse_blend_with_masks(
-        (uint32_t *)pchSource + PIXEL_COUNT - 1, 
-        pchSourceMask + PIXEL_COUNT - 1,
-        (uint32_t *)pchTarget,
-        pchTargetMask,
-        PIXEL_COUNT);
-#endif
-
-#if 0
-    __arm_2d_sve_ccca8888_blend_to_cccn888_with_opacity(
-        (uint32_t *)pchSource, 
-        //pchSourceMask + PIXEL_COUNT - 1,
-        (uint32_t *)pchTarget,
-        //pchTargetMask,
-        PIXEL_COUNT
-        ,0xFF
-    );
-#endif
-
-    //sve_tester((uint32_t *)pchSource + 20 - 1, (uint32_t *)pchTarget, pchMask + 20 - 1, 20);
-
-
-    __arm_2d_sve_ccca8888_blend_to_rgb565_and_opacity((uint32_t *)pchSource, (uint16_t *)pchTarget, PIXEL_COUNT, 0x80);
+    /*
+    sdl_sve_accc8888_blend_to_nccc888_fill_alpha
+    sdl_sve_accc8888_blend_to_nccc888_copy_alpha
+    sdl_sve_ccca8888_blend_to_cccn888_fill_alpha
+    sdl_sve_ccca8888_blend_to_cccn888_copy_alpha
+    sdl_sve_a123_blend_to_321a_fill_alpha
+    sdl_sve_a123_blend_to_321a_copy_alpha
+    sdl_sve_123a_blend_to_a321_fill_alpha
+    sdl_sve_123a_blend_to_a321_copy_alpha
+    sdl_sve_accc_blend_to_ccca_fill_alpha
+    sdl_sve_accc_blend_to_ccca_copy_alpha
+    sdl_sve_ccca_blend_to_accc_fill_alpha
+    sdl_sve_ccca_blend_to_accc_copy_alpha
+    sdl_sve_a123_blend_to_a321_fill_alpha
+    sdl_sve_a123_blend_to_a321_copy_alpha
+    sdl_sve_123a_blend_to_321a_fill_alpha
+    sdl_sve_123a_blend_to_321a_copy_alpha
+    sdl_sve_argb8888_blend_to_rgb565
+    sdl_sve_rgba8888_blend_to_rgb565
+    sdl_sve_bgra8888_blend_to_rgb565
+    sdl_sve_abgr8888_blend_to_rgb565
+    */
+    sdl_sve_a123_blend_to_321a_copy_alpha(  
+                                            pchSource, 
+                                            INPUT_BUFFER_SIZE / 2, 
+                                            pchTarget, 
+                                            OUTPUT_BUFFER_SIZE / 2, 
+                                            OUTPUT_BUFFER_SIZE / 2, 
+                                            2);
 
     SVT_PRINT_BUFFER(pchSource, INPUT_BUFFER_SIZE, uint32_t, "%08"PRIx32, 16);
-    SVT_PRINT_BUFFER(pchTarget, OUTPUT_BUFFER_SIZE, uint16_t, "%04"PRIx16, 16);
+    SVT_PRINT_BUFFER(pchTarget, OUTPUT_BUFFER_SIZE, uint32_t, "%08"PRIx32, 16);
+    //SVT_PRINT_BUFFER(pchTarget, OUTPUT_BUFFER_SIZE / 2, uint16_t, "%04"PRIx16, 16);
 
     
     free(pchSource);
@@ -144,117 +171,4 @@ int main(void) {
 
     return 0;
 }
-
-__attribute__((nonnull(1,2)))
-void sve_tester(uint32_t * __restrict pwSource, 
-                uint32_t * __restrict pwTarget, 
-                uint8_t * __restrict pchSourceMask,
-                size_t uStride)
-{
-
-#if 1
-
-    /* generate one-pass address */
-    pwSource += 1;
-    pchSourceMask += 1;
-
-    __arm_2d_sve_stride_loop_ccca8888__(uStride, vTailPred){
-
-        svbool_t vTailPredRev = svrev_b8(vTailPred);
-
-        SVT_PRINT_PRED(vTailPred, uint8_t);
-        SVT_PRINT_PRED(vTailPredRev, uint8_t);
-
-        pwSource -= __iteration_advance__;
-        pchSourceMask -= __iteration_advance__;
-
-        svuint16x4_t vSourceLow16x4 = svundef4_u16();
-        svuint16x4_t vSourceHigh16x4 = svundef4_u16();
-
-        svuint16x4_t vTargetLow16x4 = svundef4_u16();
-        svuint16x4_t vTargetHigh16x4 = svundef4_u16();
-
-        svld4ub_u16(vTailPredRev, (uint8_t *)pwSource, &vSourceLow16x4, &vSourceHigh16x4);
-        svld4ub_u16(vTailPred, (uint8_t *)pwTarget, &vTargetLow16x4, &vTargetHigh16x4);
-
-        svuint8_t vu8SourceMask = svld1_u8(vTailPredRev, pchSourceMask);
-
-        SVT_PRINT_VECTOR(vu8SourceMask, uint8_t, "0x%02"PRIx8);
-
-        svuint16_t vSourceMaskHigh = svrev(svunpkhi_u16(vu8SourceMask));
-
-        /* process low half */
-        __arm_2d_sve_pixel_ccca_foreach_chn__(vSourceHigh16x4, vTargetLow16x4,
-
-            if (__chn_idx__ == 3) {
-                SVT_PRINT_VECTOR(svrev(vSourceMaskHigh), uint16_t, "0x%04"PRIx16);
-                __svu16_target__ = vSourceMaskHigh;
-            } else {
-                SVT_PRINT_VECTOR(__svu16_source__, uint16_t, "0x%04"PRIx16);
-                __svu16_source__ = svrev(__svu16_source__);
-                
-
-                __svu16_target__ = __svu16_source__;
-            }
-
-        );
-
-        /* process high half */
-        svuint16_t vSourceMaskLow = svrev(svunpklo_u16(vu8SourceMask));
-
-        __arm_2d_sve_pixel_ccca_foreach_chn__(vSourceLow16x4, vTargetHigh16x4,
-
-            if (__chn_idx__ == 3) {
-                __svu16_target__ = vSourceMaskLow;
-                SVT_PRINT_VECTOR(svrev(vSourceMaskLow), uint16_t, "0x%04"PRIx16);
-            } else {
-                SVT_PRINT_VECTOR(__svu16_source__, uint16_t, "0x%04"PRIx16);
-                __svu16_source__ = svrev(__svu16_source__);
-                
-
-                __svu16_target__ = __svu16_source__;
-            }
-        );
-
-        svst4ub_u16(vTailPred, (uint8_t *)pwTarget, vTargetLow16x4, vTargetHigh16x4);
-
-        pwTarget += __iteration_advance__;
-    }
-
-#endif
-
-#if 0
-    svbool_t vPred = svdup_n_b8(true);
-    SVT_INIT_PRED(vPred, 0b00010101, 0b00010101, 0b00010101, 0b00010101);
-
-    svuint16_t vInput16 = svld1ub_u16(vPred, pchSource);
-
-    vInput16 = svdup_u16_m(vInput16, ~vPred, 0x00FF);
-
-    SVT_PRINT_VECTOR(vInput16, uint16_t, "0x%04"PRIx8);
-#endif
-
-#if 0
-    svuint32_t vIndex32 = svindex_u32(3, 4);
-    
-    svuint32_t vAlpha0 = svld1ub_gather_offset_u32(svptrue_b32(), pchSource, vIndex32);
-
-    pchSource += svlen_u32(vAlpha0) * sizeof(uint32_t);
-
-    svuint32_t vAlpha1 = svld1ub_gather_offset_u32(svptrue_b32(), pchSource, vIndex32);
-
-    //vAlpha0 = svrev_u32(vAlpha0);
-
-    svuint16_t vAlpha3 = svuzp1_u16(svreinterpret_u16_u32(vAlpha0),
-                                     svreinterpret_u16_u32(vAlpha1));
-
-    SVT_PRINT_VECTOR(vAlpha0, uint32_t, "0x%08"PRIx32);
-    SVT_PRINT_VECTOR(vAlpha1, uint32_t, "0x%08"PRIx32);
-
-    SVT_PRINT_VECTOR(svreinterpret_u16_u32(vAlpha0), uint16_t, "0x%04"PRIx16);
-    SVT_PRINT_VECTOR(svreinterpret_u16_u32(vAlpha1), uint16_t, "0x%04"PRIx16);
-    SVT_PRINT_VECTOR(vAlpha3, uint16_t, "0x%04"PRIx16);
-#endif
-}
-
 
